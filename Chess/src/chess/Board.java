cb1dd544283738408e81dfe7e84626256bfe0fa8
Jens2 <j.vdmeer10@hotmail.com>
2015-02-04 16:08:13 +0100

    It all makes sense now :O

--- Chess/src/chess/Board.java
+++ Chess/src/chess/Board.java
@@ -2,11 +2,11 @@ package chess;
 
 
 public class Board {
-	//Comments zijn leuk puntkomma;
+
 	public void doei(){
 		System.out.println("Bye");
 	}
 	public void hallo(){
-		
+		System.out.println("Hello");
 	}
 }
