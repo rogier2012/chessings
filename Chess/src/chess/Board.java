a6aa3d5c8bdf8cd5546a456b809a2a381c6d510a
Ron van Bree <ronvbree@hotmail.com>
2015-02-05 17:55:31 +0100

    ALS JE GVD NU IETS WIL TWELLO'EN DOE JE DAT MAAR IN EEN DAARVOOR BESTEMDE CLASS. Dikke kus <3

--- Chess/src/chess/Board.java
+++ Chess/src/chess/Board.java
@@ -3,20 +3,4 @@ package chess;
 
 public class Board {
 
-	public void doei(){
-		System.out.println("Bye");
-	}
-	public void hallo(){
-		System.out.println("Hello");
-	}
-	
-	public void aids(){
-		System.out.println("IK HOOR OOK BIJ HET PROGRAMMEERCLUBJE");
-	}
-	
-	//haha
-	
-	// DOET IE???:)
-	
-
 }
