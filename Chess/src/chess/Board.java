8590f32a8311c1d83f43c693ec296db91be46b5e
rogier2012 <r.monshouwer@student.utwente.nl>
2015-02-09 21:05:08 +0100

    Beetje board

--- Chess/src/chess/Board.java
+++ Chess/src/chess/Board.java
@@ -1,6 +1,16 @@
 package chess;
 
+import chess.pieces.*;
 
 public class Board {
-
+	private ChessPiece fields[][];
+	
+	public Board() {
+		fields = new ChessPiece[8][8];
+		
+	}
+	
+	public void reset(){
+		fields[0][0] = new Pawn();
+	}
 }
