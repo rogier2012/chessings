f786bdc899e536364bb3cd2967072a0097e0c1ad
Ron van Bree <ronvbree@hotmail.com>
2015-02-09 21:39:39 +0100

    Field + Color

--- Chess/src/chess/Board.java
+++ Chess/src/chess/Board.java
@@ -3,14 +3,11 @@ package chess;
 import chess.pieces.*;
 
 public class Board {
-	private ChessPiece fields[][];
+	private Field[][] fields = new Field[8][8];
 	
 	public Board() {
-		fields = new ChessPiece[8][8];
-		
 	}
 	
 	public void reset(){
-		fields[0][0] = new Pawn();
 	}
 }
---
+++ Chess/src/chess/Field.java
@@ -0,0 +1,5 @@
+package chess;
+
+public class Field {
+	private Color color;
+}
