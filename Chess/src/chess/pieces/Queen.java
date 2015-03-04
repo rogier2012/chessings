9b763f957bd93e98eefb516573d2ad4b5b9f6e62
rogier2012 <r.monshouwer@student.utwente.nl>
2015-03-04 17:10:47 +0100

    Auto generate gijs er uit gehaald

--- Chess/src/chess/pieces/Queen.java
+++ Chess/src/chess/pieces/Queen.java
@@ -6,12 +6,10 @@ public class Queen extends ChessPiece {
 	
 	public Queen(Color color, int rank, int file) {
 		super(color, rank, file);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public boolean isValidMove() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
