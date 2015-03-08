829ae9e4df3fee84a0477dabd4b9be409b64cb2c
GijsBeernink <g.j.beernink@student.utwente.nl>
2015-03-08 16:06:04 +0100

    0

--- Chess/src/chess/Twello.java
+++ Chess/src/chess/Twello.java
@@ -1,23 +1,65 @@
 package chess;
 
 public class Twello {
-	
-	private String antwoord = "";
-	private boolean isRonDood;
+
+	public static void main(String[] args) {
+		Ron ron = new Ron();
+		
+		ron.watIsRon();
+	}
+
 	/*
 	 * Uw uitlaatklep voor alle grappen, grollen, meligheden en gedoe. Klasse.
 	 */
+
+}
+
+class Ron {
+
+	private String antwoord = "aardbei";
+	private boolean isRonDood;
+
+	public Ron() {
+
+	}
+
 	public String waaromTwello() {
 		return antwoord;
 	}
-	
-	public void moveRon(int rageLvl){
-		if (rageLvl > 10 ){
+
+	public void moveRon(int rageLvl) {
+		if (rageLvl > 10) {
 			isRonDood = true;
 		} else {
 			rageLvl++;
 		}
-		
+
 	}
-	
+	public void watIsRon() {
+		System.out.println("Wat ben je?");
+		try {
+			Thread.sleep(2234);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		System.out.println("een beunhaas");
+		
+		weetjeWatJijBent();
+	}
+	public void weetjeWatJijBent() {
+		try {
+			Thread.sleep(2234);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		System.out.println("Beunhaaas beunhaas jij bent een beunhaasss.......");
+		// Copyright RvB
+		
+		weetjeWatJijBent();
+
+	}
 }
