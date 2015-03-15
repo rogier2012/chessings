267425b1e8235614f3750f10571e5af367d1af17
Ron van Bree <ronvbree@hotmail.com>
2015-03-15 13:58:36 +0100

    ben nu wel klaar denk ik

---
+++ Chess/src/chess/view/GameView.java
@@ -0,0 +1,16 @@
+package chess.view;
+
+import chess.model.*;
+
+public class GameView {
+	
+	/*
+	 * Prints all moves that have been done during this game
+	 */
+	public void printGame(Game game)	{
+		
+	}
+	
+	
+	
+}
