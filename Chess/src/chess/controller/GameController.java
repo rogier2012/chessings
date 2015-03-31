e191264e3f8408fda8dea788de54888a96010daa
Ron van Bree <ronvbree@hotmail.com>
2015-03-31 15:56:27 +0200

    GameController.play()

--- Chess/src/chess/controller/GameController.java
+++ Chess/src/chess/controller/GameController.java
@@ -7,51 +7,60 @@ import chess.model.players.*;
 public class GameController {
 	
 	public void play(Game game){
+		// Temporary variables
 		boolean doorgaan = true;
 		Move nextMove = null;
 		boolean isComputer;
 		boolean isHuman;
 		boolean playerDidValidMove;
 		Player currentPlayer;
-		
+		// Set up board before playing
 		game.getBoardController().reset(game.getBoard());
 		while (doorgaan){
-			playerDidValidMove = false;
-			
-			
-			currentPlayer = game.getPlayers()[game.getCurrent()];
-			isComputer = (currentPlayer instanceof ComputerPlayer);
-			isHuman = (currentPlayer instanceof HumanPlayer);
-			
-			if (isComputer)	{
-				nextMove = game.getComputerPlayerController().determineMove(game.getBoard(), (ComputerPlayer) currentPlayer);
-			}
-			else if (isHuman)	{
-				nextMove = game.getHumanPlayerController().determineMove(game.getBoard(), (HumanPlayer) currentPlayer);
-			}
-			executeMove(game, game.getBoard(), nextMove);
-			
+			// Show current board state
 			System.out.println(game.getBoardView().toString(game.getBoard()));
-			
-			
+			// Player has not done a move yet
+			playerDidValidMove = false;
+			// Get the player that has to do a move
+			currentPlayer = game.getPlayers()[game.getCurrent()];
+			// Is the player a computer?
+			isComputer = (currentPlayer instanceof ComputerPlayer);
+			// Is the player a human?
+			isHuman = (currentPlayer instanceof HumanPlayer);
+			// Continue asking the player for a move until we receive a valid one
 			while (!playerDidValidMove)	{
-				
+				// Request a move from the player
+				if (isComputer)	{
+					nextMove = game.getComputerPlayerController().determineMove(game.getBoard(), (ComputerPlayer) currentPlayer);
+				}
+				else if (isHuman)	{
+					nextMove = game.getHumanPlayerController().determineMove(game.getBoard(), (HumanPlayer) currentPlayer);
+				}
+				// Check if the move is valid
+				playerDidValidMove = isValidMove(game, nextMove);
 			}
-			
-			
+			// Execute the valid move
+			executeMove(game, nextMove);
+			// Check if we should continue the game
 			doorgaan = !gameOver(game);
 		}
 	}
 	
-	public void executeMove(Game game, Board board, Move move)	{
-		game.getBoardController().executeMove(move, board);
+	/*
+	 * Execute the move
+	 */
+	public void executeMove(Game game, Move move)	{
+		game.getBoardController().executeMove(move, game.getBoard());
 	}
 	
-	public boolean gameOver(Game game)	{
-		
-		
+	private boolean gameOver(Game game)	{
+		// TODO : Checks if the game ended
 		return false;
 	}
 	
+	private boolean isValidMove(Game game, Move move)	{
+		// TODO : Checks if the suggested move is valid
+		return true;
+	}
 	
 }
