cb218090b0edafc0e1b51e205b4c26b6cf0f33be
Ron van Bree <ronvbree@hotmail.com>
2015-08-31 12:03:53 +0200

    ALLES MOET KAPOT

--- Chess/src/chess/controller/players/PlayerController.java
+++
@@ -1,9 +0,0 @@
-package chess.controller.players;
-
-import chess.model.Board;
-import chess.model.players.*;
-import chess.model.moves.Move;
-
-public abstract class PlayerController {
-	
-}
--- Chess/src/chess/model/strategies/Strategy.java
+++
@@ -1,9 +0,0 @@
-package chess.model.strategies;
-
-import chess.model.moves.*;
-import chess.model.*;
-
-public interface Strategy {
-	public String getName();
-	public Move determineMove(Board board, Color color);
-}
